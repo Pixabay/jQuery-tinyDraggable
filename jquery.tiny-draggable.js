6a0db0ae97510eec778a8e0c66736ec14c57049d
SimonSteinberger <simon.steinberger@outlook.com>
2014-07-13 11:28:48 +0200

    Minor changes to make minified code smaller

--- jquery.tiny-draggable.js
+++ jquery.tiny-draggable.js
@@ -1,5 +1,5 @@
-/*!
-	jQuery tinyDraggable v1.0.0-beta - 2014-06-25
+/*
+	jQuery tinyDraggable v1.0.0-beta
     Copyright (c) 2014 Simon Steinberger / Pixabay
     GitHub: https://github.com/Pixabay/jQuery-tinyDraggable
     More info: http://pixabay.com/blog/posts/p-52/
@@ -13,11 +13,11 @@
             var dx, dy, el = $(this), handle = settings.handle ? $(settings.handle, el) : el;
             handle.on({
                 mousedown: function(e){
-                    if (settings.exclude && $.inArray(e.target, $(settings.exclude, el))>-1) return;
+                    if (settings.exclude && ~$.inArray(e.target, $(settings.exclude, el))) return;
                     var os = el.offset(); dx = e.pageX-os.left, dy = e.pageY-os.top;
-                    $(document).on('mousemove.draggable', function(e){ el.offset({top: e.pageY-dy, left: e.pageX-dx}); });
+                    $(document).on('mousemove.drag', function(e){ el.offset({top: e.pageY-dy, left: e.pageX-dx}); });
                 },
-                mouseup: function(e){ $(document).off('mousemove.draggable'); }
+                mouseup: function(e){ $(document).off('mousemove.drag'); }
             });
         });
     }
